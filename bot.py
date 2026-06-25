d0106f8aa18d10d2c835b22e6dc8d8a2f0a5f179
adi26raj <adi.26raj@gmail.com>
2026-06-25 21:34:06 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot.run(TOKEN)
