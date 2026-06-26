e7fe6d0dc14f273be2117e5d6375139228d5f9d9
adi26raj <adi.26raj@gmail.com>
2026-06-26 17:26:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
+import os
+import threading
+from flask import Flask
+
 import discord
 from discord.ext import commands
-from flask import Flask
-import threading
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flower Registry Bot is running!"
+    return "Flower Registry Bot is online!"
 
 def run_web():
     port = int(os.environ.get("PORT", 10000))
@@ -21,9 +22,21 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} slash commands.")
+    except Exception as e:
+        print(e)
+
     print(f"Logged in as {bot.user}")
+
+
+import commands_file
+
+commands_file.setup(bot)
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
