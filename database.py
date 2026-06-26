9fcd27a1deb3c05331734a64de648150837d24ff
adi26raj <adi.26raj@gmail.com>
2026-06-26 17:43:55 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 PLAYERS_FILE = "players.json"
 FLOWERS_FILE = "flowers.json"
@@ -23,32 +24,63 @@ def save_json(filename, data):
         json.dump(data, f, indent=4)
 
 
+# ---------- PLAYERS ----------
+
 def get_players():
     return load_json(PLAYERS_FILE)
 
 
-def save_players(data):
-    save_json(PLAYERS_FILE, data)
+def save_players(players):
+    save_json(PLAYERS_FILE, players)
 
+
+def register_player(discord_id, ign):
+    players = get_players()
+    players[str(discord_id)] = {"ign": ign}
+    save_players(players)
+
+
+def get_ign(discord_id):
+    players = get_players()
+    user = players.get(str(discord_id))
+    if not user:
+        return None
+    return user["ign"]
+
+
+# ---------- FLOWERS ----------
 
 def get_flowers():
     return load_json(FLOWERS_FILE)
 
 
-def save_flowers(data):
-    save_json(FLOWERS_FILE, data)
+def save_flowers(flowers):
+    save_json(FLOWERS_FILE, flowers)
 
+
+# ---------- REGISTRY ----------
 
 def get_registry():
     return load_json(REGISTRY_FILE)
 
 
-def save_registry(data):
-    save_json(REGISTRY_FILE, data)
-def player_ign(discord_id):
-    players = get_players()
+def save_registry(registry):
+    save_json(REGISTRY_FILE, registry)
 
-    if str(discord_id) not in players:
-        return None
 
-    return players[str(discord_id)]["ign"]
+def claim_flower(flower_name, ign, discord_id):
+
+    registry = get_registry()
+
+    if flower_name in registry:
+        return False, registry[flower_name]["owner"]
+
+    registry[flower_name] = {
+        "owner": ign,
+        "discord_id": str(discord_id),
+        "claimed_at": datetime.utcnow().isoformat()
+    }
+
+    save_registry(registry)
+
+    return True, ign
